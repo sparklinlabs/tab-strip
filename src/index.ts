f51d505f019671bd586457130a04093b2b4dfdfb
Elisée Maurer <elisee@sparklinlabs.com>
2015-05-25 19:26:40 +0200

    Port to TypeScript, bump to 1.1.0

---
+++ src/index.ts
@@ -0,0 +1,126 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+import { EventEmitter } from "events";
+
+class TabStrip extends EventEmitter {
+
+  tabsRoot: HTMLOListElement;
+
+  constructor(container: HTMLElement) {
+    super();
+
+    this.tabsRoot = document.createElement("ol");
+    this.tabsRoot.classList.add("tab-strip");
+    container.appendChild(this.tabsRoot);
+
+    this.tabsRoot.addEventListener("mousedown", this._onTabMouseDown);
+    this.tabsRoot.addEventListener("mouseup", this._onTabMouseUp);
+  }
+
+  _onTabMouseUp = (event: MouseEvent) => {
+    let tabElement = <HTMLElement>event.target;
+
+    // Only handle middle-click and ignore clicks outside any tab
+    if (event.button !== 1 || tabElement.parentElement !== this.tabsRoot) return;
+
+    this.emit("closeTab", tabElement);
+  };
+
+  _onTabMouseDown = (event: MouseEvent) => {
+    let tabElement = <HTMLElement>event.target;
+
+    // Only handle left-click
+    if (event.button !== 0 || tabElement.parentElement !== this.tabsRoot) return;
+
+    this.emit("activateTab", tabElement);
+
+    // Tab reordering
+    let tabRect = tabElement.getBoundingClientRect();
+    let leftOffsetFromMouse = tabRect.left - event.clientX;
+    let hasDragged = false;
+
+    tabElement.classList.add("dragged");
+
+    // FIXME: Hard-coded border?
+    tabElement.style.width = `${tabRect.width + 1}px`;
+
+    // NOTE: set/releaseCapture aren"t supported in Chrome yet
+    // hence the conditional call
+    if ((<any>tabElement).setCapture != null) (<any>tabElement).setCapture();
+
+    let tabPlaceholderElement = <HTMLLIElement>document.createElement("li");
+    tabPlaceholderElement.style.width = `${tabRect.width}px`;
+    tabPlaceholderElement.className = "drop-placeholder";
+    tabElement.parentElement.insertBefore(tabPlaceholderElement, tabElement.nextSibling);
+
+    let updateDraggedTab = (clientX: number) => {
+      let tabsRootRect = this.tabsRoot.getBoundingClientRect();
+
+      let tabLeft = Math.max(Math.min(clientX + leftOffsetFromMouse, tabsRootRect.right - tabRect.width), tabsRootRect.left);
+      if (hasDragged || Math.abs(tabLeft - tabRect.left) >= 10) {
+        hasDragged = true;
+      } else {
+        tabLeft = tabRect.left;
+      }
+
+      tabElement.style.left = `${tabLeft}px`;
+
+      if (tabLeft < tabPlaceholderElement.getBoundingClientRect().left) {
+        let otherTabElement = tabPlaceholderElement;
+        while (true) {
+          otherTabElement = <HTMLLIElement>tabPlaceholderElement.previousSibling;
+          if (otherTabElement === tabElement) otherTabElement = <HTMLLIElement>otherTabElement.previousSibling;
+          if (otherTabElement == null) break;
+
+          let otherTabCenter = otherTabElement.getBoundingClientRect().left + otherTabElement.getBoundingClientRect().width / 2;
+          if (otherTabCenter < tabLeft) break;
+
+          otherTabElement.parentElement.insertBefore(tabPlaceholderElement, otherTabElement);
+        }
+      } else {
+        let otherTabElement = tabPlaceholderElement;
+        while (true) {
+          otherTabElement = <HTMLLIElement>tabPlaceholderElement.nextSibling;
+          if (otherTabElement === tabElement) otherTabElement = <HTMLLIElement>otherTabElement.nextSibling;
+          if (otherTabElement == null) break;
+
+          let otherTabCenter = otherTabElement.getBoundingClientRect().left + otherTabElement.getBoundingClientRect().width / 2;
+          if (tabLeft + tabRect.width < otherTabCenter) break;
+
+          otherTabElement.parentElement.insertBefore(tabPlaceholderElement, otherTabElement.nextSibling);
+        }
+      }
+
+      if (tabPlaceholderElement.nextSibling === tabElement) {
+        tabElement.parentElement.insertBefore(tabPlaceholderElement, tabElement.nextSibling);
+      }
+    };
+
+    let onDragTab = (event: MouseEvent) => { updateDraggedTab(event.clientX) };
+
+    let onDropTab = (event: MouseEvent) => {
+      // NOTE: set/releaseCapture aren't supported in Chrome yet
+      // hence the conditional call
+      if ((<any>tabElement).releaseCapture != null) (<any>tabElement).releaseCapture();
+
+      if (tabPlaceholderElement.parentElement != null) {
+        this.tabsRoot.replaceChild(tabElement, tabPlaceholderElement);
+      } else {
+        this.tabsRoot.appendChild(tabElement);
+      }
+
+      tabElement.classList.remove("dragged");
+      tabElement.style.left = "";
+      tabElement.style.width = "";
+
+      document.removeEventListener("mousemove", onDragTab);
+      document.removeEventListener("mouseup", onDropTab);
+    };
+
+    updateDraggedTab(event.clientX);
+    document.addEventListener("mousemove", onDragTab);
+    document.addEventListener("mouseup", onDropTab);
+  };
+}
+
+export = TabStrip;
